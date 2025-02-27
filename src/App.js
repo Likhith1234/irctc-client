a58dec3447b2fe6ca8525953f8011965858d274f
Likhith1234 <kesavlikku@gmail.com>
2025-02-27 11:44:01 +0530

    mini changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div className="App">
-      <HashRouter basename='/irctc-client'>
+      <HashRouter basename='/'>
         <NavBar/>
         <Routes>
           <Route path='/' element={<Home />} />
